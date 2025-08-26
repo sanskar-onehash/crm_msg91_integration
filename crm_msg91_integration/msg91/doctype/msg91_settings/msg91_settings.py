19c2cc8a72949595844016ca85bbbe4378c1990f
Sanskar Soni <sanskarsoni300@gmail.com>
2025-08-26 14:24:18 +0530

    fix(otp): return api calls result in msg91_settings
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- crm_msg91_integration/msg91/doctype/msg91_settings/msg91_settings.py
+++ crm_msg91_integration/msg91/doctype/msg91_settings/msg91_settings.py
@@ -9,7 +9,7 @@ from crm_msg91_integration.msg91.integration import service as msg91
 class MSG91Settings(Document):
 
     def send_otp(self, *args, **kwargs):
-        msg91.send_otp(*args, **kwargs)
+        return msg91.send_otp(*args, **kwargs)
 
     def verify_otp(self, *args, **kwargs):
-        msg91.verify_otp(*args, **kwargs)
+        return msg91.verify_otp(*args, **kwargs)
